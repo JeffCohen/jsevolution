2bdfff43e52ae6dd3103779a019f3b76fd1dab62
Jeff Cohen <cohen.jeff@gmail.com>
2017-07-18 19:35:28 -0500

    Small update

--- stock_quote_jquery_1.js
+++ stock_quote_jquery_1.js
@@ -1,11 +1,53 @@
 window.portfolio = { symbols: [], total: 0.0 }
 
+
 $(function() {
+
+  //  Initialize stock list
+  $.getJSON("https://ucexchange.herokuapp.com/stocks.json", function( stocks ) {
+    for (var i = 0; i < stocks.length; ++i) {
+      var tr_element = $("<tr></tr>");
+      var td1 = $('<td data-symbol="' + stocks[i].symbol + '">' + stocks[i].symbol + "</td>");
+      var td2 = $("<td>" + stocks[i].rounded_price + "</td>");
+      var add_button = $('<td><a href="" data-symbol="'+ stocks[i].symbol + '" class="add"><i class="fa fa-lg fa-plus-circle"></i></a></td>');
+      tr_element.append(td1);
+      tr_element.append(td2);
+      tr_element.append(add_button);
+      $('#all_stocks tbody').append(tr_element);
+    }
+  });
+
   $('#quote').on('submit', function(event) {
     event.preventDefault();
     var symbol = $("input[name='symbol']").val();
     getStockPrice(symbol);
   })
+
+  $(".sortable").on('click', function(event) {
+    event.preventDefault();
+    console.debug("sorting...")
+    var table_id = $(event.target).closest('table')[0].id;
+    var sortkey = $(event.target).data('sortkey');
+    var table_body = $('#' + table_id + ' tbody')[0]
+    $(table_body).html("");
+    $.getJSON("https://ucexchange.herokuapp.com/stocks.json", function( stocks ) {
+      if (sortkey == 'price') {
+        stocks.sort(compareByPrice);
+      } else {
+        stocks.sort(compareBySymbol);
+      }
+      for (var i = 0; i < stocks.length; ++i) {
+        var tr_element = $("<tr></tr>");
+        var td1 = $('<td data-symbol="' + stocks[i].symbol + '">' + stocks[i].symbol + "</td>");
+        var td2 = $("<td>" + stocks[i].rounded_price + "</td>");
+        var add_button = $('<td><a href="" data-symbol="'+ stocks[i].symbol + '" class="add"><i class="fa fa-lg fa-plus-circle"></i></a></td>');
+        tr_element.append(td1);
+        tr_element.append(td2);
+        tr_element.append(add_button);
+        $(table_body).append(tr_element);
+      }
+    });
+  });
 
   $('body').on('click', '.fa-minus-circle', function(event) {
     event.preventDefault();
@@ -89,10 +131,6 @@ $(function() {
   });
 });
 
-function showPrice(symbol, price) {
-  $("#price").html("<h1>" + symbol + ": " + price);
-};
-
 function getStockPrice(symbol) {
   $.getJSON( "https://ucexchange.herokuapp.com/stocks.json", function( stocks ) {
     for (var i = 0; i < stocks.length; ++i) {
@@ -105,3 +143,17 @@ function getStockPrice(symbol) {
     }
   });
 };
+
+function compareByPrice(stock_1, stock_2) {
+  return (stock_1.rounded_price - stock_2.rounded_price);
+}
+
+function compareBySymbol(stock_1, stock_2) {
+  if (stock_1.symbol > stock_2.symbol) {
+    return 1;
+  }
+  if (stock_1.symbol < stock_2.symbol) {
+    return -1;
+  }
+  return 0;
+}
